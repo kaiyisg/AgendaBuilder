4b5ebbbd148cb18a18039eda6949c4eb93294177
Lee Kai Yi <kaiyisg@yahoo.com.sg>
2016-03-09 00:10:44 +0100

    finishing up layouts for the first page

--- app/src/main/java/com/interactionprog/agendabuilder/android/MainActivity.java
+++ app/src/main/java/com/interactionprog/agendabuilder/android/MainActivity.java
@@ -1,16 +1,23 @@
 package com.interactionprog.agendabuilder.android;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.interactionprog.agendabuilder.R;
 import com.interactionprog.agendabuilder.android.view.ActivityEditorDialog;
@@ -50,29 +57,63 @@ public class MainActivity extends AppCompatActivity {
                         .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 final View activityEditorView = inflater.inflate(R.layout.editor_activity_dialog, null);
 
+                //initializing buttons and widgets in dialog
                 Button saveActivity = (Button)activityEditorView.findViewById(R.id.button);
                 Button cancelActivity = (Button)activityEditorView.findViewById(R.id.button2);
+                final EditText nameActivity = (EditText)activityEditorView.findViewById(R.id.editText);
+                final EditText lengthActivity = (EditText)activityEditorView.findViewById(R.id.editText2);
+                final EditText descriptionActivity = (EditText)activityEditorView.findViewById(R.id.editText3);
 
+                //creating dialog box
                 final Dialog dialog = new Dialog(v.getContext());
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                 dialog.setContentView(activityEditorView);
 
+                //changing dialog box to go towards the right
+                Window window = dialog.getWindow();
+                WindowManager.LayoutParams wlp = window.getAttributes();
+                wlp.gravity = Gravity.END;
+                wlp.flags &= ~WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                window.setAttributes(wlp);
+
                 ActivityEditorDialog activityEditorDialog =
                         new ActivityEditorDialog(activityEditorView, null);
-                //v.findViewById(R.id.editor_activity_dialog_id)
 
                 //setting what happens when we save the activity
                 saveActivity.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
 
-                        //checking if all fields are set up properly, else cancel saving
+                        String nameData = nameActivity.getText().toString();
+                        String lengthData = lengthActivity.getText().toString();
+                        String descriptionData = nameActivity.getText().toString();
 
-                        //getting all the information from the fields
+                        if (nameData.matches("")) {
 
-                        //save into the old activity if it exists
+                            Toast.makeText(AgendaBuilderApplication.getAppContext(),
+                                    "Please enter a title to proceed!", Toast.LENGTH_SHORT).show();
 
-                        //save into a new activity if it does not previously exist
+                        } else if (lengthData.matches("")) {
+
+                            Toast.makeText(AgendaBuilderApplication.getAppContext(),
+                                    "Please enter a time period to proceed!", Toast.LENGTH_SHORT).show();
+
+                        } else if (descriptionData.matches("")) {
+
+                            Toast.makeText(AgendaBuilderApplication.getAppContext(),
+                                    "Please enter a description to proceed!", Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            //getting all the information from the fields
+
+                            //save into the old activity if it exists
+
+                            //save into a new activity if it does not previously exist
+
+
+                        }
+
 
                         dialog.cancel();
 
@@ -106,4 +147,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    private void alertDialogPromptBuilder(String alert, View v){
+        new AlertDialog.Builder(v.getContext())
+                .setTitle("Enter details")
+                .setMessage(alert)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                }).show();
+    }
+
 }
